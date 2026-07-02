5821b3bab382c19466280dfa5af52f37e2d02acb
Mahmoud <mahmoudalshehyby@gmail.com>
2026-07-02 20:02:59 +0300

    Update mobile view slider gaps

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3131105065"
+    serviceWorkerVersion: "1624642491"
   }
 });
